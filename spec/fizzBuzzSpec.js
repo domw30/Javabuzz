cdb9aa032a76de8fbda65a9081c93228aae02484
domw30 <domw30@gmail.com>
2019-04-29 18:52:26 +0100

    refactor spec file

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -8,33 +8,25 @@ describe('Javabuzz', function() {
     it('divisible by 3', function() {
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
-  });
 
-  describe('knows when a number is NOT', function() {
-    it('divisible by 3', function() {
-      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    it('divisible by 15', function() {
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
     });
-  });
-
-  describe('knows when a number is', function() {
-    it('divisible by 5', function() {
-      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
-    });
-  });
-
-  describe('knows when a number is NOT', function() {
-    it('divisible by 5', function() {
-      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
-    });
-  });
-
-  describe('knows when a number is', function() {
+    
     it('divisible by 15', function() {
       expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
     });
   });
 
   describe('knows when a number is NOT', function() {
+    it('divisible by 3', function() {
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+    });
+
     it('divisible by 15', function() {
       expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
     });
